87c40a5328380f8a956c0e0af7339171f56badf3
face2005 <golofaev73@gmail.com>
2023-05-11 07:21:53 +0300

    Update form_wp.php
    22

--- form_wp.php
+++ form_wp.php
@@ -1,1 +1,26 @@
-<?php echo "55"; ?>
+<form action="#">   
+    <div class="formBlock">
+
+        <h2 class="titleForm">Залиште заявку</h2>
+        <div class="descFormModal">наш менеджер зв'яжеться з Вами найближчим часом...</div>
+
+        <div class="formRow formInputName">
+            <span class="wpcf7-form-control-wrap" data-name="name-client"><input size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required nameField" aria-required="true" aria-invalid="false" placeholder="Ім'я" value="" type="text" name="name-client"></span>
+        </div>
+
+        <div class="formRow formInputTel">
+            <span class="wpcf7-form-control-wrap" data-name="tel-client">
+                <input size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required telField" aria-required="true" aria-invalid="false" placeholder="Телефон" value="" type="text" name="tel-client">
+            </span>
+        </div>
+
+        <div class="formRow popapFormInputDate">
+            <span class="wpcf7-form-control-wrap" data-name="time-client">
+                <input size="40" class="wpcf7-form-control wpcf7-text timeField" aria-invalid="false" placeholder="Зручний час для розмови" value="" type="text" name="time-client">
+            </span>
+        </div>
+
+        <div class="formRow formSubmit">
+            <input class="wpcf7-form-control has-spinner wpcf7-submit" type="submit" value="Відправити"><span class="wpcf7-spinner"></span>
+        </div>    
+</form>
