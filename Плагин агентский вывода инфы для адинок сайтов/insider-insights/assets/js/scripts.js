b4fcd53db0b6237671034c07797be609b489e117
face2005 <golofaev73@gmail.com>
2024-02-14 13:54:58 +0200

    123

--- Плагин агентский вывода инфы для адинок сайтов/insider-insights/assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".sliderLogin", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: ".button-next-sa",
-        prevEl: ".button-prev-sa",
-    },
-});
